939dae05b9741150b21031d8eb7318435a674918
Duziem Ugoji <tickettoduzz@gmail.com>
2022-05-09 17:11:39 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontend-challenge-duziem-ugoji',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
